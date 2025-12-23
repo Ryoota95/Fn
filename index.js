416e85f170c2e0a241411cc8d2f4f0bb306b8303
Ryoota95 <rayyadabiyazid@gmail.com>
2025-12-23 19:25:50 +0700

    finaltask

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const port = 3000
+
+
+
+// view engine
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'src/views'))
+
+// static file
+app.use(express.static(path.join(__dirname, 'src/public')))
+
+// route
+app.get('/', (req, res) => {
+  res.render('home', {
+    name: 'Rayyan',
+    role: 'student',
+    location: 'Koja, jakarta utara, Indonesia',
+    available: true
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Server jalan di port 3000')
+})
---
+++ src/public/js/theme.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("darkToggle");
+
+  if (!btn) return; // jaga-jaga kalau layout beda
+
+  // load theme terakhir
+  if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+    btn.textContent = "☀️";
+  }
+
+  btn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    const isDark = document.body.classList.contains("dark");
+    localStorage.setItem("theme", isDark ? "dark" : "light");
+
+    btn.textContent = isDark ? "☀️" : "🌙";
+  });
+});
