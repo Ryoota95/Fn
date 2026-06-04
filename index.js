1fcc6f81cf5123d5f05723eb66d4d67882d96390
Ryoota95 <rayyadabiyazid@gmail.com>
2026-06-04 13:02:40 +0700

    update portofolio again

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, 'src/public')))
 // route
 app.get('/', (req, res) => {
   res.render('home', {
-    name: 'Alisa',
+    name: 'Ray',
     role: 'student',
     location: 'Koja, jakarta utara, Indonesia',
     available: true
